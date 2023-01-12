a842470df8ccfec8be07591841583ea1edd17838
MaximNep <nep23001@byui.edu>
2023-01-12 21:19:04 +0300

    first commit

---
+++ tire_volume.py
@@ -0,0 +1,9 @@
+# from math import *
+# w = float(input('enter the width: '))
+# a = float(input('enter the aspect ratio of the tire: '))
+# d = float(input('enter the diameter of the wheel: '))
+# v = (pi * w ** 2 * a * (w * a + 2540 * d)) / (10 ** 10)
+# print(v)
+
+print(3 * 2 - 17 * 2 * 2 / (2 + 5))
+print(3 * 2 - (17 * 2 * 2) / 2 + 5)
